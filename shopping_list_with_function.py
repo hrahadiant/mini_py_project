cc4a48eea12e2616cc1811e77a8afae676add9ec
hrahadiant <tomo@jackslabs.com>
2016-09-10 14:10:36 +0700

    clean up the code

--- shopping_list_with_function.py
+++ shopping_list_with_function.py
@@ -1,32 +1,36 @@
 # shopping list with function
 
-# instruction
-print("Follow the short instruction: ")
-print("enter a word SHOW, to show list")
-print("enter a word HELP, to see how to use this apps")
-print("enter a word DONE, to completely the list")
-print("\n")
-print("enter a new shopping list")
+print("enter a new list for you...")
 
-def mylist():
-    shopping_list = []
-    while True:
-        items = input("> ")
-        if items == "SHOW":
-            if len(shopping_list) == 0:
-                print("you have nothing in your list")
-                mylist()
-            elif len(shopping_list) != 0:
-                for i in shopping_list:
-                    print(i)
-                mylist()
-        elif items == "DONE":
-            break
-        elif items == "HELP":
-            print("add one list at once a time.")
-            print("type SHOW, to see full list")
-            print("type DONE, to completely list")
-            print("type HELP, to see how to use this app")
-            mylist()
-        else:
-            shopping_list.append(items)
+shopping_list = []
+
+
+def show_help():
+    print("typing SHOW, to show the current list")
+    print("typing HELP, to see how to use this apps")
+    print("typing DONE, to print out your list")
+
+
+def show_list():
+    print("Here is your list:")
+    for item in shopping_list:
+        print(item)
+
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("You just added {}. List now has {} items.".format(item, len(shopping_list)))
+
+show_help()
+
+while True:
+    items = input("> ")
+    if items == "SHOW":
+        show_list()
+        continue
+    elif items == "DONE":
+        break
+    elif items == "HELP":
+        show_help()
+        continue
+    add_to_list(items)
