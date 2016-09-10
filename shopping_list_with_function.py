81f1eba7c95b2921ca53c9418ce3658e9284ad2c
hrahadiant <tomo@jackslabs.com>
2016-09-10 14:13:24 +0700

    added new comment/instructions

--- shopping_list_with_function.py
+++ shopping_list_with_function.py
@@ -1,22 +1,24 @@
-# shopping list with function
+# basic shopping list with function and clean code
+# has 3 command in this apps
+# SHOW, HELP, and DONE
 
 print("enter a new list for you...")
 
 shopping_list = []
 
-
+# function to show the help
 def show_help():
     print("typing SHOW, to show the current list")
     print("typing HELP, to see how to use this apps")
     print("typing DONE, to print out your list")
 
-
+# function for show the list
 def show_list():
     print("Here is your list:")
     for item in shopping_list:
         print(item)
 
-
+# function to add a new item into the list
 def add_to_list(item):
     shopping_list.append(item)
     print("You just added {}. List now has {} items.".format(item, len(shopping_list)))
